ba4bcf291733a727305db211a60007abffc89a52
Razil Shaikh <shaikhrazil@gmail.com>
2019-03-17 15:35:04 +0530

    Add support for representing and observing events through LiveData.

---
+++ app/src/main/java/dev/razil/lilhub/common/event/Event.kt
@@ -0,0 +1,72 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Razil Shaikh
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package dev.razil.lilhub.common.event
+
+/**
+ * Used as a wrapper for data that is exposed via a LiveData that represents an event.
+ *
+ * [Read more about this.](https://medium.com/google-developers/livedata-with-snackbar-navigation-and-other-events-the-singleliveevent-case-ac2622673150)
+ */
+open class Event<out T>(private val content: T) {
+
+    var consumed = false
+        private set // Allow external read but not write
+
+    /**
+     * Consumes the content if it's not been consumed yet.
+     * @return The unconsumed content or `null` if it was consumed already.
+     */
+    fun consume(): T? {
+        return if (consumed) {
+            null
+        } else {
+            consumed = true
+            content
+        }
+    }
+
+    /**
+     * @return The content whether it's been handled or not.
+     */
+    fun peek(): T = content
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Event<*>
+
+        if (content != other.content) return false
+        if (consumed != other.consumed) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = content?.hashCode() ?: 0
+        result = 31 * result + consumed.hashCode()
+        return result
+    }
+}
